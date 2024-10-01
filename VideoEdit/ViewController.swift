f19e785c826947df85fcba6a51750eec7555dc17
baekhyeoneun <hebaek@kocowa.com>
2024-10-01 19:15:03 +0900

    Setting For video data.

--- VideoEdit/ViewController.swift
+++ VideoEdit/ViewController.swift
@@ -6,15 +6,55 @@
 //
 
 import UIKit
+import PhotosUI
 
 class ViewController: UIViewController {
 
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Add Video", for: .normal)
+        button.titleLabel?.textColor = .white
+        button.backgroundColor = .red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        setUpUI()
+        button.addTarget(self, action: #selector(test), for: .touchUpInside)
     }
+    
+    @objc func test(_ sender: UIButton) {
+        var config = PHPickerConfiguration()
+        config.selectionLimit = 0 // 제한 없음
+        config.filter = .any(of: [.videos])
 
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
 
 }
 
+extension ViewController: PHPickerViewControllerDelegate {
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        //TODO: CollectionView에 video 전달하기.
+    }
+    
+}
+
+//MARK: Set Up UI
+private extension ViewController {
+    func setUpUI() {
+        view.addSubview(button)
+        [
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+        ].forEach { $0.isActive = true }
+    }
+}
